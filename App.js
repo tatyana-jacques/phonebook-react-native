8caf8449ca6ecb6d183bba91c2486f4502803ca3
Tatyana Jacques <tatyana_jacques@estudante.sesisenai.org.br>
2022-08-12 20:44:32 -0300

    Atualiazação do projeto.

--- App.js
+++ App.js
@@ -1,44 +1,82 @@
 import { SafeAreaView, StatusBar, StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import kim from "./assets/kim.jpeg"
+import thurston from "./assets/thurston.jpeg"
+import rita from "./assets/rita.jpeg"
+import smith from "./assets/smith.jpeg"
 
 export default function App() {
+
+const myContacts = [
+  {
+    id: 1,
+    name: "Kim Gordom",
+    contact: "11 11111-1111",
+    photo: kim
+  },
+
+  {
+    id: 2,
+    name: "Thurston More",
+    contact: "22 22222-2222",
+    photo: thurston
+  },
+
+  {
+    id: 3,
+    name: "Rita Lee",
+    contact: "33 33333-3333",
+    photo: rita
+  },
+
+  {
+    id: 4,
+    name: "Robet Smith",
+    contact: "22 22222-2222",
+    photo: smith
+  },
+
+  {
+    id: 5,
+    name: "Thurston Moretttttttttttttttttttttttttttttttttttttttttttt",
+    contact: "22 22222-2222",
+    photo: thurston
+  },
+
+
+
+]
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor={"#0f4c5c"} />
+      <ScrollView showVerticalScrollIndicator={false}>
       <Text style = {styles.title}>Meus contatos</Text>
+        
+        {myContacts.map ((myContacts)=>(
+            <View style = {styles.contacts}>
+            <Image style = {styles.photos} source = {myContacts.photo}/>
+            <View style = {styles.contactsText}>
+            <Text style = {styles.text} numberOfLines = {1} ellipsizeMode = {"tail"}>{myContacts.name}</Text>
+            <Text style = {styles.numbers}>{myContacts.contact}</Text>
+            </View>
+          </View>
+          ))
+        }
+
+
+
+    
       
-      <View style = {styles.contacts}>
-        <Image source = {kim} style = {styles.photos}/>
-        <View style = {styles.contactsText}>
-        <Text style = {styles.text}>Kim Gordon</Text>
-        <Text style = {styles.numbers}>+55 55 55555 5555</Text>
-        </View>
-      </View>
+     
 
-      <View style = {styles.contacts}>
-        <Image source = {kim} style = {styles.photos}/>
-        <View style = {styles.contactsText}>
-        <Text style = {styles.text}>Kim Gordon</Text>
-        <Text style = {styles.numbers}>+55 55 55555 5555</Text>
-        </View>
-      </View>
+     
 
-      <View style = {styles.contacts}>
-        <Image source = {kim} style = {styles.photos}/>
-        <View style = {styles.contactsText}>
-        <Text style = {styles.text}>Kim Gordon</Text>
-        <Text style = {styles.numbers}>+55 55 55555 5555</Text>
-        </View>
-      </View>
+     
 
-      <View style = {styles.contacts}>
-        <Image source = {kim} style = {styles.photos}/>
-        <View style = {styles.contactsText}>
-        <Text style = {styles.text}>Kim Gordon</Text>
-        <Text style = {styles.numbers}>+55 55 55555 5555</Text>
-        </View>
-      </View>
-      
+     
+
+    
+      </ScrollView>
       
     </SafeAreaView>
   )
@@ -54,7 +92,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 35,
     color: "#0f4c5c",
-    textAlign: 'center',
+    alignSelf: "center",
     margin: "5%",
 
   },
@@ -72,19 +110,20 @@ const styles = StyleSheet.create({
     borderColor: "#0f4c5c",
     borderStyle: "solid",
     borderWidth: 2,
-   
-
+    width: "90%",
+    
   },
+
   photos: {
     width: 50,
     height: 50,
-
+    borderRadius: 50,
   },
+
   contactsText: {
     flexDirection: "column",
     marginLeft:"10%",
-    
-
+    width:"70%",
   },
 
   text: {
